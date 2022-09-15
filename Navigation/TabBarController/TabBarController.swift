13ba8ea334d86aff303df194582f112844791072
Olga Tsaturova <miss_olga@inbox.ru>
2022-09-15 22:50:33 +0300

    Add gitignore

---
+++ Navigation/TabBarController/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  Navigation
+//
+//  Created by Mac on 15.09.2022.
+//
+
+import SwiftUI
+
+struct TabBarController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarController_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarController()
+    }
+}
